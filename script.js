56dc6cb0747e341f9ac0eb9381e22e45bd656d40
AnilKumar3494 <90452951+AnilKumar3494@users.noreply.github.com>
2023-01-11 02:00:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-var Typed = new Typed(".typing",{
-    strings:["", "Web Developer", "Java Developer","Content Writer"],
-    typeSpeed:100,
-    backSpeed:60,
-    loop:true
+var Typed = new Typed(".typing", {
+  strings: [" ", "Web Developer", "Java Developer", "Content Writer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
 });
+
+let nav_listEl = document.querySelector("#nav_list");
+function asideNavBar() {
+  nav_listEl.classList.toggle("nav_transform");
+}
+
+let nav_itemEL = document.querySelector("#nav_item");
+function selected() {
+  nav_itemEL.classList.toggle("active");
+}
